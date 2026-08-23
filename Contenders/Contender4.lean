b3719b88d71f961515731db9fafeb852ff8def0c
Kim Morrison <477956+kim-em@users.noreply.github.com>
2026-08-23 22:21:11 +1000

    Add contender 42 (#7)

--- Contenders/Contender4.lean
+++ Contenders/Contender4.lean
@@ -3,8 +3,10 @@ Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Contenders.Contender0
-import Contenders.Contender1
-import Contenders.Contender2
 import Contenders.Contender3
-import Contenders.Contender4
+
+/-- Submitted by Kim Morrison. -/
+def contender_4 : Nat := 42
+
+theorem contender_3_lt_contender_4 : contender_3 < contender_4 := by
+  norm_num [contender_3, contender_4]
