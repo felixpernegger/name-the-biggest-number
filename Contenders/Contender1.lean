f60830cbc7dee501e27f77c7a8c1ae58de00cc86
Kim Morrison <477956+kim-em@users.noreply.github.com>
2026-08-23 22:11:58 +1000

    Add contender 1 (#4)

--- Contenders/Contender1.lean
+++ Contenders/Contender1.lean
@@ -4,4 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
 import Contenders.Contender0
-import Contenders.Contender1
+
+/-- Submitted by Kim Morrison. -/
+def contender_1 : Nat := 1
+
+theorem contender_0_lt_contender_1 : contender_0 < contender_1 := by
+  norm_num [contender_0, contender_1]
