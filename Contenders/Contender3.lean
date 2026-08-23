719fc96e7c69bd3f0b1ebf3d8868ec093a1c6f14
Kim Morrison <477956+kim-em@users.noreply.github.com>
2026-08-23 22:18:19 +1000

    Add contender 2 times 2 (#6)

--- Contenders/Contender3.lean
+++ Contenders/Contender3.lean
@@ -3,7 +3,10 @@ Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Contenders.Contender0
-import Contenders.Contender1
 import Contenders.Contender2
-import Contenders.Contender3
+
+/-- Submitted by Kim Morrison. -/
+def contender_3 : Nat := 2 * 2
+
+theorem contender_2_lt_contender_3 : contender_2 < contender_3 := by
+  norm_num [contender_2, contender_3]
