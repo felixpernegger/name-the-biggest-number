26d31e51d1baafacbbfccd6bbd6e4b2788aea4bc
Kim Morrison <477956+kim-em@users.noreply.github.com>
2026-08-23 22:14:51 +1000

    Add contender 2 (#5)

--- Contenders/Contender2.lean
+++ Contenders/Contender2.lean
@@ -3,6 +3,10 @@ Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
-import Contenders.Contender0
 import Contenders.Contender1
-import Contenders.Contender2
+
+/-- Submitted by Kim Morrison. -/
+def contender_2 : Nat := 2
+
+theorem contender_1_lt_contender_2 : contender_1 < contender_2 := by
+  norm_num [contender_1, contender_2]
